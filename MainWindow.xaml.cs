e3de1a6ef369838e3fa4bc6a32f22fc7803fd050
JavierPs287 <your-email@example.com>
2024-12-09 21:01:26 +0100

    Carga de Fondo de Pantalla Login

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,13 @@ namespace ProyectoIPo
         public MainWindow()
         {
             InitializeComponent();
+            string imagePath = @"Recursos\Login\FondoPantallaLogin.jpg";
+
+            Uri imageUri = new Uri(imagePath, UriKind.Relative); // Usa UriKind.Relative si la ruta es relativa
+            BitmapImage bitmap = new BitmapImage(imageUri);
+
+            // Asignar la imagen como fondo
+            this.Background = new ImageBrush(bitmap);
         }
     }
 }
